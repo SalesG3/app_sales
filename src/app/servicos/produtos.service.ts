751f48713fa529b62454fe7821a62c4799ef951d
SalesG3 <contabilgs3@gmail.com>
2025-08-18 22:38:48 -0300

    OK

--- src/app/servicos/produtos.service.ts
+++ src/app/servicos/produtos.service.ts
@@ -36,4 +36,28 @@ export class ProdutosService {
 
     return data
   }
+
+  async salvarProduto(dataRow : object){
+
+    let request = await fetch(environment.API + `produtos/${this.sessao.ID_ENTIDADE}/insert`, {
+      method: "POST",
+      headers: this.headers,
+      body: JSON.stringify(dataRow)
+    })
+
+    let data = await request.json()
+
+    return data
+  }
+
+  async teste(){
+    let request = await fetch(environment.API + 'teste', {
+      method: "GET",
+      headers: this.headers
+    })
+
+    let data = request.json()
+
+    return data
+  }
 }
